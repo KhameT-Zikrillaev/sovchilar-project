c6924208e8642d9ec3d25ae70736f6dce2c276e1
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-12-20 13:57:25 +0500

    fixed

--- src/components/Reklama2/index.jsx
+++ src/components/Reklama2/index.jsx
@@ -9,15 +9,16 @@ import instatext from "../../assets/images/insta-text.png";
 export default function Reklama() {
   return (
     <>
-    <a href="">
+    <a href="https://www.instagram.com/mi_market__?igsh=MWZmeWhvNW92YXh1NQ==">
       <div className="reklama h-[35px] bg-white text-center fixed w-full top-0 z-50 flex items-center justify-center">
-        <img  className="animatepulseicon mx-2 h-[20px]" src={Male} alt="" />
+        <img  className="animatepulseicon hidden sm:inline mx-2 h-[20px]" src={Male} alt="" />
         <div className="animate-text-bged   text-[12px] md:text-sm flex items-center">
-          <span>Haqiqiy sevgingizni toping va baxtli oila quring!</span>
+          <span className="hidden xl:inline">Haqiqiy sevgingizni toping va baxtli oila quring!</span>
           <img className="h-[15px] hidden md:inline ml-2" src={Rings} alt="" />
-          <span className="hidden md:inline ml-2hidden md:inline ml-2">Sevgi va oila topish imkoniyati – bu yerda.</span>
+          <span className=" ml-2">Sevgi va oila topish imkoniyati – bu yerda.</span>
+          <span> <strong>mi_market__</strong></span>
         </div>
-        <img  className="animatepulseicon mx-2 h-[20px]" src={Female} alt="" />
+        <img  className="animatepulseicon hidden sm:inline mx-2 h-[20px]" src={Female} alt="" />
           <img className="w-10 overflow-hidden animate-pulse" src={insta} alt="Instagram Icon" />
           <img  className="w-20 animate-pulse"  src={instatext} alt="Instagram Text" />
       </div>
--- src/components/sertficatedmodal/index.jsx
+++ src/components/sertficatedmodal/index.jsx
@@ -7,7 +7,7 @@ const index = ({ isOpen, onClose }) => {
 
     return (
         <div className="fixed z-50 inset-0 bg-gradient-to-br from-gray-900 via-black to-gray-800 bg-opacity-70 flex justify-center items-center">
-            <div className="p-10 md:w-[50%]  rounded-xl shadow-2xl relative md:w-4/5 md:max-h-[85vh] overflow-y-auto border-4">
+            <div className=" p-1 md:p-10 md:w-[50%]  h-[80%] rounded-xl shadow-2xl relative md:w-4/5 md:max-h-[85vh] overflow-y-auto">
                 <button
                     className="absolute top-4 right-4 text-3xl font-bold text-gray-200 hover:text-red-600 transition-transform transform hover:scale-110 focus:outline-none"
                     onClick={onClose}
