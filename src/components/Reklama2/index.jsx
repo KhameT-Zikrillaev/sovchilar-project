fea4319f139a1f7400edea4c0a9cbae7f7e11f9b
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-12-20 15:28:19 +0500

    new fixed

--- src/components/Reklama2/index.jsx
+++ src/components/Reklama2/index.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { FaHeart } from "react-icons/fa"; // Импортируем иконку сердца
+import React from "react"; // Импортируем иконку сердца
 import "./Reklama.css";
 import Male from "../../assets/images/MaleWedding.png";
 import Female from "../../assets/images/FemaleWedding.png";
 import Rings from "../../assets/images/Rings.png";
 import insta from "../../assets/images/insta.png";
 import instatext from "../../assets/images/insta-text.png";
+import { useTranslation } from 'react-i18next' 
 export default function Reklama() {
+  const { t } = useTranslation();
   return (
     <>
     <a href="https://www.instagram.com/mi_market__?igsh=MWZmeWhvNW92YXh1NQ==">
       <div className="reklama h-[35px] bg-white text-center fixed w-full top-0 z-50 flex items-center justify-center">
         <img  className="animatepulseicon hidden sm:inline mx-2 h-[20px]" src={Male} alt="" />
         <div className="animate-text-bged   text-[12px] md:text-sm flex items-center">
-          <span className="hidden xl:inline">Haqiqiy sevgingizni toping va baxtli oila quring!</span>
+          <span className="hidden xl:inline">{t("reklama.text1")}</span>
           <img className="h-[15px] hidden md:inline ml-2" src={Rings} alt="" />
-          <span className=" ml-2">Sevgi va oila topish imkoniyati – bu yerda.</span>
+          <span className=" ml-2">{t("reklama.text2")}</span>
           <span> <strong>mi_market__</strong></span>
         </div>
         <img  className="animatepulseicon hidden sm:inline mx-2 h-[20px]" src={Female} alt="" />
