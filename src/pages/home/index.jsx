71b233ea4b56aba917c7ec42f7bdc3226693e74f
Ziyoviddin880 <ziyoviddinabdurashidov7@gmail.com>
2025-01-06 18:24:51 +0500

    None telephone Number

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import FirstHomePageIntro from './pages/FirstHomePageIntro'
-import SecondHomePageSearch from './pages/SecondHomePageSearch'
+import React from "react";
+import FirstHomePageIntro from "./pages/FirstHomePageIntro";
+import SecondHomePageSearch from "./pages/SecondHomePageSearch";
+import Comments from "./pages/Comments/Comments";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 mt-[30px]">
       <FirstHomePageIntro />
+      <Comments />
       <SecondHomePageSearch />
     </div>
-  )
+  );
 }
