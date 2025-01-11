bbccb33904afeda5bfb7754d4e14be4726d093f8
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2025-01-11 17:49:39 +0500

    test profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -62,9 +62,23 @@ const Profile = () => {
               onClick={() => {
                 clearUser()
                 navigate("/")}}
-              className=" bg-rose-500 text-white px-6 py-3 rounded-lg hover:bg-rose-600 transition font-medium flex-grow flex-basic-0 flex-shrink-[200px]"
+              className="bg-rose-500 text-white px-6 py-3 rounded-lg hover:bg-rose-600 transition font-medium flex-grow flex-basic-0 flex-shrink-[200px] flex items-center justify-center gap-2"
             >
-             {t('Profile.btn.logout')}
+             <span>{t('Profile.btn.logout')}</span>
+             <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                />
+              </svg>
             </button>
             
           </div>
