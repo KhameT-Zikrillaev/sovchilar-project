f8a49a713e0f688f2776d67327d6f22479adfff1
Otabek <turayevotabek195@gmail.com>
2025-01-11 15:13:23 +0500

    fix: sadada

--- src/pages/home/pages/SecondHomePageSearch/index.jsx
+++ src/pages/home/pages/SecondHomePageSearch/index.jsx
@@ -128,7 +128,7 @@ export default function SecondHomePageSearch() {
       />
 
       {isLoading ? (
-        <Loading />
+        <div className="h-[300px] flex justify-center items-center"><Loading /></div>
       ) : (
         <>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
